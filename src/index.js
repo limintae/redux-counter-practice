ad7b2c69e0e1d4de7f2b22393d6e8365628616c2
limintae <limintae@gmail.com>
2019-05-06 01:58:28 +0900

    redux-counter first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './containers/App'
 import './index.css';
-import App from './App';
+
+// Redux 관련 불러오기
+import { createStore } from 'redux'
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 스토어 생성 Redux 개발자도구
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// 스토어 생성
+//const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
